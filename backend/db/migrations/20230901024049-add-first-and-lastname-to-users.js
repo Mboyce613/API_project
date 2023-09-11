d8d4fd5cf72c50b85f0ece43a60fd85fa4e982c8
Michael <michael.a.boyce999@gmail.com>
2023-09-11 13:16:52 -0700

    i think i got it now

--- backend/db/migrations/20230901024049-add-first-and-lastname-to-users.js
+++ backend/db/migrations/20230901024049-add-first-and-lastname-to-users.js
@@ -8,15 +8,11 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.addColumn('Users', 'firstName', { type: Sequelize.DataTypes.STRING, allowNull: false, },options);
+    options.tableName = "Users";
 
-    await queryInterface.addColumn('Users', 'lastName', { type: Sequelize.DataTypes.STRING, allowNull: false, },options);
+    await queryInterface.addColumn(options, 'firstName', { type: Sequelize.DataTypes.STRING, allowNull: false, });
+
+    await queryInterface.addColumn(options, 'lastName', { type: Sequelize.DataTypes.STRING, allowNull: false, });
 
   },
 
@@ -29,9 +25,9 @@ module.exports = {
      * await queryInterface.dropTable('users');
      */
 
-    await queryInterface.removeColumn('Users', 'firstName', { type: Sequelize.DataTypes.STRING, allowNull: false, },options);
+    await queryInterface.removeColumn(options, 'firstName', { type: Sequelize.DataTypes.STRING, allowNull: false, });
 
-    await queryInterface.removeColumn('Users', 'lastName', { type: Sequelize.DataTypes.STRING, allowNull: false, },options);
+    await queryInterface.removeColumn(options, 'lastName', { type: Sequelize.DataTypes.STRING, allowNull: false, });
 
   }
 };
