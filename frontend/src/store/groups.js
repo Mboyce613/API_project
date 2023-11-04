979b7b38e8a756d662a7d7701e08c6797fecea2d
michael <michael.a.boyce999@gmail.com>
2023-11-04 14:46:15 -0700

    done

---
+++ frontend/src/store/groups.js
@@ -0,0 +1,8 @@
+import { Dispatch } from "react";
+
+/** Action Type Constants: */
+export const LOAD_GROUP = 'groups/LOAD_GROUP';
+export const CREATE_GROUP = 'groups/CREATE_GROUP';
+export const READ_GROUP = 'groups/READ_GROUP';
+export const UPDATE_GROUP = 'groups/UPDATE_GROUP';
+export const DELETE_GROUP = 'groups/DELETE_GROUP';
